a252a64ea2b5edd67608658139dc09d7e6704c4d
Ben Wickerson <ben.wickerson@gmail.com>
2013-04-29 12:25:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CricketApp::Application.config.secret_key_base = '8dc28b803977971b8dd020c3f4e160053f97b5e0fc14145a00b84f6bc8c3790f128eb203bf7614ccb86ca07f53c5dae30fadc57459dc59adaf1eff07004e57dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CricketApp::Application.config.session_store :encrypted_cookie_store, key: '_cricket_app_session'
