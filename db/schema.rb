585b8c541db4a4cea17c3947713a5cc2c68de809
Ben Wickerson <ben.wickerson@gmail.com>
2013-05-03 10:31:50 +0100

    User model started

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130429133215) do
+ActiveRecord::Schema.define(version: 20130503090902) do
 
-  create_table "articles", force: true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.text     "content"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
